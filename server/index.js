cc97f6e9be93fb22d6e9fd6cbbefa00b88b31171
Bruno Austin <brunoaugustine2@gmail.com>
2023-01-16 17:25:56 +0100

    first commit, project and client queries

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db.js')
+const colors = require('colors')
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Live @ port ${PORT}...`.yellow.bold)
+})
