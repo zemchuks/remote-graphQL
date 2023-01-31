65c4891214dcd0d5f70d0a7747c28e7d2e365428
Bruno Austin <brunoaugustine2@gmail.com>
2023-01-31 14:08:31 +0100

    new

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import Header from './components/Header'
+import Clients from './components/Clients'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
-
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+})
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+    <ApolloProvider client={client}>
+    <Header />
+      <main>
+        <div className=''>
+          <Clients />
+        </div>
+      </main>
+        
+      </ApolloProvider>
+   
+    </>
+    
   )
 }
 
