4cb904b8ce3d71414a0a7be4e11a087d3e46f10a
Bruno Austin <brunoaugustine2@gmail.com>
2023-02-04 17:19:03 +0100

    add spinner and table to display items

--- frontend/src/components/ClientRow.jsx
+++ frontend/src/components/ClientRow.jsx
@@ -1,21 +1,21 @@
+import { FaTrash } from 'react-icons/fa'
 
 const ClientRow = ({ client }) => {
   return (
     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {client.name}
-                </th>
-                <td className="px-6 py-4">
-                    {client.email}
-                </td>
-                <td className="px-6 py-4">
-                    {client.phone}
-                </td>
-                <td className="px-6 py-4">
-                <button type="button" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Delete</button>
-
-                </td>
-    </tr>
+    <th scope="row" className="px-6 py-4 font-medium text-gray-900 bg-white white:text-dark whitespace-nowrap white:bg-slate-500">
+    {client.name}
+    </th>
+    <td className="px-6 py-4 bg-white white:text-dark whitespace-nowrap white:bg-slate-50">
+    {client.email}
+    </td>
+    <td className="px-6 py-4 bg-white white:text-dark whitespace-nowrap white:bg-slate-50">
+    {client.phone}
+    </td>
+    <td className="px-6 py-4 bg-white white:text-dark whitespace-nowrap white:bg-slate-50">
+    <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"><FaTrash /></button>
+    </td>
+</tr>
   )
 }
 
--- frontend/src/components/Clients.jsx
+++ frontend/src/components/Clients.jsx
@@ -1,22 +1,14 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import { PaperClipIcon } from '@heroicons/react/20/solid'
 import ClientRow from './ClientRow'
+import { GET_CLIENTS } from '../queries/clientQueries'
+import Spinner from './Spinner'
 
-const GET_CLIENTS = gql`
-  query getClients {
-      clients {
-        id,
-        name,
-        email,
-        phone
-      }
-    }
-  `
 
 export default function Clients() {
 
   const {loading, error, data} = useQuery(GET_CLIENTS)
-  if (loading) return <p>Loading...</p>
+  if (loading) return <Spinner />
   if (error) return <p>Something went wrong</p>
 
   return (
@@ -85,12 +77,15 @@ export default function Clients() {
     //   </div>
     // </div>
 
-    
 
-<div className="relative overflow-x-auto">
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-white-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table className=" w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <caption className="p-5 text-lg font-semibold text-left text-gray-900 bg-slate-50 white:text-dark white:bg-slate-500">
+            Our products
+            <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
+        </caption>
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 white:bg-slate-200 white:text-dark-400">
+        <tr>
                 <th scope="col" className="px-6 py-3">
                     Product name
                 </th>
@@ -107,12 +102,14 @@ export default function Clients() {
             </tr>
         </thead>
         <tbody>
-          {data.clients.mp((client) => (
+          {data.clients.map((client) => (
             <ClientRow key={client.id} client={client} />
           ))}
         </tbody>
+           
     </table>
 </div>
+
 
 
   )
